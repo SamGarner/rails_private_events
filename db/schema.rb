927443058b9a2296946996a719a612dfd54af959
SamGarner <samuel.w.garner@gmail.com>
2021-01-11 18:27:37 -0600

    Fix: Commit 'missing' updated db/schema.rb file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_172845) do
+ActiveRecord::Schema.define(version: 2021_01_11_204105) do
 
   create_table "events", force: :cascade do |t|
     t.date "date"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2021_01_11_172845) do
     t.text "description"
   end
 
+  create_table "invitations", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "guest_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_invitations_on_event_id"
+    t.index ["guest_id"], name: "index_invitations_on_guest_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -28,4 +37,6 @@ ActiveRecord::Schema.define(version: 2021_01_11_172845) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "invitations", "events"
+  add_foreign_key "invitations", "users", column: "guest_id"
 end
