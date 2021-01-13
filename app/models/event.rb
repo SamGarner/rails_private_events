1a1a9a30ec5433da1c4a855deb5f67312a057f4c
SamGarner <samuel.w.garner@gmail.com>
2021-01-12 20:19:13 -0600

    Style: Replace double-quotes in Event model to satisfy Rubocop. Add line break to users show.html.erb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,19 +1,11 @@
 class Event < ApplicationRecord
   default_scope { order(date: :desc) }
-  scope :upcoming, ->(now) { where("date >= ?", now).order(date: :asc) }
-  scope :past, ->(now) { where("date < ?", now) }
+  scope :upcoming, ->(now) { where('date >= ?', now).order(date: :asc) }
+  scope :past, ->(now) { where('date < ?', now) }
 
   validates :date, :location, presence: true
 
   belongs_to :creator, class_name: 'User'
   has_many :invitations
   has_many :guests, through: :invitations
-
-  # def self.past
-  #   select { |event| event.date < DateTime.now }
-  # end
-
-  # def self.upcoming
-  #   select { |event| event.date >= DateTime.now }
-  # end
 end
